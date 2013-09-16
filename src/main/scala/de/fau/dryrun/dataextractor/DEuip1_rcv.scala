b893551a26939c05610d0d9020171764986a9389
Moritz 'Morty' Strübe <morty@gmx.net>
2013-09-16 18:31:52 +0200

    Idmap must support concurrent access

--- src/main/scala/de/fau/dryrun/dataextractor/DEuip1_rcv.scala
+++ src/main/scala/de/fau/dryrun/dataextractor/DEuip1_rcv.scala
@@ -20,14 +20,15 @@ class DEuip1_rcv extends DEuip1 {
 					val splitlines = lines.map(_.split(" "))
 					//urn:fau:0x000a: MAC 00:12:74:00:0e:d5:30:12 Contiki 2.6 started. Node id is set to 10.
 					val idlines = splitlines.filter(x => {x.size == 12 && x(1).equals("MAC")})
-					var idmap = {for(l <- idlines) yield {
+					//idMap maps the output id to the real node id
+					var idMap = {for(l <- idlines) yield {
 						import DataExtractor._
 						val mid = extract(l(0)).get
 						val id = l(2).split(":").last.hex
 						id -> mid
 					}}.toMap
 					
-					DEuip1_rcv.backupMap ++= idmap
+					DEuip1_rcv.backupMap ++= idMap
 					
 					//log.debug("IDmap: " + idmap.map(x => {x._1 + " " + x._2}).mkString(", ") )
 					
@@ -39,20 +40,21 @@ class DEuip1_rcv extends DEuip1 {
 						val ctr = rcvMap.getOrElse(src, 0)
 						rcvMap += src -> (ctr + 1) 
 					}
-					for(key <- rcvMap.keys) if(!idmap.contains(key)) {
+					for(key <- rcvMap.keys) if(!idMap.contains(key)) {
 						val b = getBackup(key)
 						if(b.isDefined) {
 							if(!missKeyReported) {
 								missKeyReported = true
 								log.debug("Missing key  \""  + key + "\" in " + filename +". No more missing keys will be reported for this file.")
 							}
-							idmap = idmap + (key -> b.get)
+							idMap = idMap + (key -> b.get)
 						} else {
 							log.error("Missing key  \""  + key + "\" in " + filename + " Could not Recover")
 							return Vector[Data]()
 						}
 					}
-					rcvMap.map(x => new Result(idmap(x._1),"meta_packrcv", x._2)).toVector
+					DEuip1_rcv.backupMap.map(x => new Result(x._2,"meta_packrcv", rcvMap.getOrElse(x._1, 0).toLong)).toVector
+					rcvMap.map(x => new Result(idMap(x._1),"meta_packrcv", x._2)).toVector
 				}
 			}
 				
@@ -63,7 +65,8 @@ class DEuip1_rcv extends DEuip1 {
 }
 
 object DEuip1_rcv{
-	val backupMap = collection.mutable.Map[Int, Int]()
+	import collection.JavaConversions._
+	val backupMap:collection.concurrent.Map[Int, Int] = new java.util.concurrent.ConcurrentHashMap[Int, Int]()
 }
 
 trait DEuip1_rcvSim extends DEuip1_rcv {
